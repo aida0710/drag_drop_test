688fb60c54c1f61da8c65d565499773773788e07
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-20 21:45:43 +0900

    patternを分けた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,34 @@
-'use client';
-
 import React from 'react';
+import {Card, CardBody, CardHeader} from '@nextui-org/react';
+import Link from 'next/link';
+import {Chip} from '@nextui-org/chip';
 
-export default function Page() {
+export default async function Page() {
     return (
-        <div className='h-screen w-full'>
-            <ul>
-                <li>
-                    <a href='/pattern-1'>pattern1</a>
-                </li>
-                <li>
-                    <a href='/pattern-2'>pattern2</a>
-                </li>
-            </ul>
+        <div className='mt-32 flex justify-center'>
+            <div className='border-default-200 space-x-3 rounded border p-12'>
+                <Link href={'/pattern-1'}>
+                    <Card radius='sm'>
+                        <CardHeader>
+                            <h3>パターン 1へ</h3>
+                        </CardHeader>
+                        <CardBody className="space-y-3">
+                            <Chip>react-dnd</Chip>
+                            <Chip>react-dnd-html5-backend</Chip>
+                        </CardBody>
+                    </Card>
+                </Link>
+                <Link href={'/pattern-2'}>
+                    <Card radius='sm'>
+                        <CardHeader>
+                            <h3>パターン 2へ</h3>
+                        </CardHeader>
+                        <CardBody>
+                            <Chip>dnd-kit</Chip>
+                        </CardBody>
+                    </Card>
+                </Link>
+            </div>
         </div>
     );
 }
