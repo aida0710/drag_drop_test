1d856acf34222be0a6cfb55c04d0a4c5e1fdb17a
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-22 03:57:04 +0900

    右クリックでメニューを表示させないよう

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -3,15 +3,12 @@
 import React from 'react';
 import {DropZone} from '@/app/(index)/DropZone';
 import {IndexMenubar} from '@/app/(index)/components/index-menubar';
-import {RightClickMenu} from '@/app/(index)/components/right-click-menu';
 
 export default function Page() {
     return (
         <div className='h-screen w-full'>
             <IndexMenubar />
-            <RightClickMenu>
-                <DropZone />
-            </RightClickMenu>
+            <DropZone />
         </div>
     );
 }
