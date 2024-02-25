7a77557f5fc88bc5f00de7539a66781618ae40c5
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-26 03:51:36 +0900

    データコンテキストを親コンポーネントで生成するように

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -4,12 +4,23 @@ import React from 'react';
 import {IndexMenubar} from '@/app/(index)/components/index-menubar';
 import 'reactflow/dist/style.css';
 import {FlowContents} from '@/app/(index)/flow-contents';
+import {DataContext} from '@/app/(index)/flow/context/data-context';
+import {BackgroundVariant, Edge, Node} from 'reactflow';
+import {IDataSettings} from '@/app/(index)/flow/context/IDataSettings';
 
 export default function Page() {
+    const [nodes, setNodes] = React.useState<Node[]>([]);
+    const [edges, setEdges] = React.useState<Edge[]>([]);
+    const [settings, setSettings] = React.useState<IDataSettings>({
+        backgroundVariant: BackgroundVariant.Lines,
+    });
+
     return (
-        <div className='w-full'>
-            <IndexMenubar />
-            <FlowContents />
-        </div>
+        <DataContext.Provider value={{nodes, edges, settings, setNodes, setEdges, setSettings}}>
+            <div className='w-full'>
+                <IndexMenubar />
+                <FlowContents />
+            </div>
+        </DataContext.Provider>
     );
 }
