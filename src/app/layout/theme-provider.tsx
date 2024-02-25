0715005517c455d4ad934eb3da161ea14c067a2c
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-26 06:41:50 +0900

    型エラーを修正

--- src/app/layout/theme-provider.tsx
+++ src/app/layout/theme-provider.tsx
@@ -1,9 +1,25 @@
 'use client';
 
 import * as React from 'react';
+import {ReactNode} from 'react';
 import {ThemeProvider as NextThemesProvider} from 'next-themes';
-import {type ThemeProviderProps} from 'next-themes/dist/types';
 
-export function ThemeProvider({children, ...props}: ThemeProviderProps) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+interface Props {
+    attribute: string;
+    defaultTheme: string;
+    enableSystem: boolean;
+    disableTransitionOnChange: boolean;
+    children: ReactNode;
 }
+
+export const ThemeProvider = ({attribute, defaultTheme, enableSystem, disableTransitionOnChange, children}: Props) => {
+    return (
+        <NextThemesProvider
+            attribute={attribute}
+            defaultTheme={defaultTheme}
+            enableSystem={enableSystem}
+            disableTransitionOnChange={disableTransitionOnChange}>
+            {children}
+        </NextThemesProvider>
+    );
+};
