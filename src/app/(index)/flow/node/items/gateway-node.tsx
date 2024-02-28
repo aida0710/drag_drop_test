efb2c211b0fa712e5066d2971da56f2bc2d41234
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-29 08:34:58 +0900

    customノードのデザインを修正

--- src/app/(index)/flow/node/items/gateway-node.tsx
+++ src/app/(index)/flow/node/items/gateway-node.tsx
@@ -4,13 +4,16 @@ import {Label} from "@/shadcn/ui/label";
 import {Input} from "@/shadcn/ui/input";
 
 interface GatewayNodeProps {
+    id: string;
     data: {
         label: string;
-        input_data: string;
+        ip_address: string;
+        subnet_mask: string;
     };
 }
 
-export const GatewayNode = ({data}: GatewayNodeProps) => {
+export const GatewayNode = ({id,data}: GatewayNodeProps) => {
+
     const onChange = useCallback((event: ChangeEvent<HTMLInputElement>): void => {
         console.log(event.target.value);
     }, []);
@@ -18,14 +21,25 @@ export const GatewayNode = ({data}: GatewayNodeProps) => {
     return (
         <div className="px-4 py-1 shadow-md rounded-md bg-white border border-black">
             <Handle type="target" position={Position.Top} className="w-3 h-3 !bg-blue-400 rounded-sm"/>
-            <div className="text-black">
-                <Label className="text-xs">{data.label}</Label>
+            <div className="text-black mb-2">
+                <div>
+                    <Label className="text-xs">{data.label}</Label>
+                    <Label className="text-xs ml-1 text-gray-400">#{id}</Label>
+                </div>
+                <p className="text-sm">IPアドレス</p>
                 <Input
                     autoComplete='off'
                     id="text"
                     name="text"
                     onChange={onChange}
-                    className="nodrag h-5"/>
+                    className="nodrag h-5 bg-white"/>
+                <p className="text-sm">サブネットマスク</p>
+                <Input
+                    autoComplete='off'
+                    id="text"
+                    name="text"
+                    onChange={onChange}
+                    className="nodrag h-5 bg-white"/>
             </div>
             <Handle type="source" position={Position.Bottom} className="w-3 h-3 !bg-teal-500 rounded-sm"/>
         </div>
