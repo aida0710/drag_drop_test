a29e2d2fe5d67305fea26fc6a18d0ecc174b5f60
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-29 10:22:48 +0900

    サイドバーを追加

---
+++ src/app/(index)/components/side-menu.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {CustomNode} from '@/app/(index)/flow/node/custom-node';
+
+export const SideMenu = () => {
+    const onDragStart = (event: React.DragEvent<HTMLDivElement>, nodeType: string) => {
+        event.dataTransfer.setData('application/reactflow', nodeType);
+        event.dataTransfer.effectAllowed = 'move';
+    };
+
+    return (
+        <aside className='overflow-y-auto p-4'>
+            <div className='mb-4 text-sm'>アイテム一覧</div>
+            <div
+                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
+                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'Gateway')}
+                draggable>
+                Gateway Node
+            </div>
+            <div
+                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
+                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'L3Switch')}
+                draggable>
+                L3 Switch Node
+            </div>
+            <div
+                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
+                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'L2Switch')}
+                draggable>
+                L2 Switch Node
+            </div>
+            <div
+                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
+                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'Server')}
+                draggable>
+                Server Node
+            </div>
+            <div
+                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
+                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'Firewall')}
+                draggable>
+                Firewall Node
+            </div>
+        </aside>
+    );
+};
+
+export const NodeTypes = {
+    Gateway: CustomNode,
+    L3Switch: CustomNode,
+    L2Switch: CustomNode,
+    Server: CustomNode,
+    Firewall: CustomNode,
+};
