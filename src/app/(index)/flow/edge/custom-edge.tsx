0393d8599a584ce7f9eda04718913f62593d8d3c
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-01 08:52:35 +0900

    データセーブに対応。 選択式でラインを変えられるように

--- src/app/(index)/flow/edge/custom-edge.tsx
+++ src/app/(index)/flow/edge/custom-edge.tsx
@@ -1,8 +1,8 @@
 import {BezierEdge, Edge, EdgeLabelRenderer, EdgeProps, getStraightPath, useReactFlow} from 'reactflow';
 import React from 'react';
 import {EdgeParameters} from '@/app/(index)/flow/edge/EdgeParameters';
+import {Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue} from '@/shadcn/ui/select';
 import {EnumEdgeTypes, EnumEdgeTypesMap} from '@/app/(index)/flow/edge/EdgeTypes';
-import {Button} from '@/shadcn/ui/button';
 
 /**
  * CustomEdgeは、Nodeに比べ制約が多く、EdgePropsを使用する必要がある
@@ -23,27 +23,6 @@ export const CustomEdge = ({id, data, sourceX, sourceY, targetX, targetY}: EdgeP
         targetX,
         targetY,
     });
-
-    const onEdgeClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
-        event.preventDefault();
-        setEdges((prevEdges: Edge[]): Edge[] => {
-            return prevEdges.map((edge: Edge): Edge => {
-                if (edge.id === id) {
-                    const edgeKeys = Object.keys(EnumEdgeTypesMap);
-                    const currentTypeIndex = edgeKeys.indexOf(edge.data.type);
-
-                    if (currentTypeIndex < edgeKeys.length - 1) {
-                        // Update to next EnumEdgeTypes if current type is not the last element
-                        edge.data.type = EnumEdgeTypesMap[edgeKeys[currentTypeIndex + 1] as keyof typeof EnumEdgeTypesMap];
-                    } else {
-                        // If current type is the last element of EnumEdgeTypes, then update to EnumEdgeTypes.Category5
-                        edge.data.type = EnumEdgeTypes.Category5;
-                    }
-                }
-                return {...edge};
-            });
-        });
-    };
 
     return (
         <>
@@ -68,12 +47,43 @@ export const CustomEdge = ({id, data, sourceX, sourceY, targetX, targetY}: EdgeP
                         pointerEvents: 'all',
                     }}
                     className='nodrag nopan'>
-                    <Button
-                        onClick={(event: React.MouseEvent<HTMLButtonElement>): void => {
-                            onEdgeClick(event);
+                    <Select
+                        onValueChange={(value: EnumEdgeTypes | 'delete'): void => {
+                            if (value === 'delete') {
+                                setEdges((edges: Edge[]) => edges.filter((edge: Edge): boolean => edge.id !== id));
+                            } else {
+                                setEdges((edges: Edge[]) =>
+                                    edges.map((edge: Edge): Edge => {
+                                        if (edge.id === id) {
+                                            return {
+                                                ...edge,
+                                                data: {
+                                                    ...edge.data,
+                                                    type: value,
+                                                },
+                                            };
+                                        }
+                                        return edge;
+                                    }),
+                                );
+                            }
                         }}>
-                        {data.type}
-                    </Button>
+                        <SelectTrigger>
+                            <SelectValue placeholder={data.type} />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                {Object.keys(EnumEdgeTypesMap).map((key: string) => {
+                                    return <SelectItem value={key}>{key}</SelectItem>;
+                                })}
+                                <SelectItem
+                                    value='delete'
+                                    className='text-red-400'>
+                                    Delete Line
+                                </SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
                 </div>
             </EdgeLabelRenderer>
         </>
