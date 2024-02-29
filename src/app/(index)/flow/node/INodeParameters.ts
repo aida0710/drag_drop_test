a3101f86f26a9da5d8c222f1bdd7cdf9e294b006
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-29 10:21:53 +0900

    不要なファイルを削除

--- src/app/(index)/flow/node/INodeParameters.ts
+++
@@ -1,9 +0,0 @@
-import {NodeTypes} from '@/app/(index)/flow/node/NodeTypes';
-
-export interface INodeParameters {
-    node_type: NodeTypes;
-    picture: string;
-    picture_width: number;
-    picture_height: number;
-    description: string;
-}
