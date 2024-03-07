c74899623f3929c5a2bba5d99b9ffdbb59ceaa95
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-07 12:14:07 +0900

    時間処理を関数か

--- src/app/(index)/utils/utils.ts
+++ src/app/(index)/utils/utils.ts
@@ -1,3 +1,21 @@
 export function genNodeId(): string {
     return Math.random().toString(36).slice(-8);
 }
+
+export function transformUtcDateToJstWithMinutes() {
+    let date: Date = new Date();
+    let year: number = date.getFullYear();
+    let month: string = ('0' + (date.getMonth() + 1)).slice(-2);
+    let day: string = ('0' + date.getDate()).slice(-2);
+    let hours: string = ('0' + date.getHours()).slice(-2);
+    let minutes: string = ('0' + date.getMinutes()).slice(-2);
+    let seconds: string = ('0' + date.getSeconds()).slice(-2);
+    return {
+        year: year,
+        month: month,
+        day: day,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds,
+    };
+}
