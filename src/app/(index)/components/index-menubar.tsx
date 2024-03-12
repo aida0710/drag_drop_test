29b52101d2a4b83632f57405f7623c8685b14e28
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-12 12:26:31 +0900

    バックアップ関係の項目を追加

--- src/app/(index)/components/index-menubar.tsx
+++ src/app/(index)/components/index-menubar.tsx
@@ -7,6 +7,12 @@ import {BackgroundVariantButton} from '@/app/(index)/components/menu-items/setti
 import {MiniMapButton} from '@/app/(index)/components/menu-items/setting/mini-map-button';
 import React from 'react';
 import {ExecuteComponents} from '@/app/(index)/components/menu-items/execute/execute-components';
+import {BackupEnableButton} from '@/app/(index)/components/menu-items/local-save/backup-enable-button';
+import {BackupIntervalInput} from '@/app/(index)/components/menu-items/local-save/backup-interval-input';
+import {BackupButton} from '@/app/(index)/components/menu-items/local-save/backup-button';
+import {BackupHistoryModal} from '@/app/(index)/components/menu-items/local-save/backup-history-modal';
+import {Label} from '@/shadcn/ui/label';
+import {Comment} from '@/app/(index)/components/menu-items/utils/comment';
 
 export const IndexMenubar = () => {
     return (
@@ -18,6 +24,24 @@ export const IndexMenubar = () => {
                     <ExportButton />
                     <MenubarSeparator />
                     <DataResetButton />
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>Local Save</MenubarTrigger>
+                <MenubarContent>
+                    <BackupEnableButton />
+                    <BackupIntervalInput />
+                    <BackupButton />
+                    <div className='rounded-sm hover:bg-accent hover:text-accent-foreground'>
+                        <MenubarItem className='grid w-full max-w-sm items-center gap-1.5'>
+                            <Label>Backup History</Label>
+                            <Comment comment='最大25個までバックアップを閲覧出来ます。' />
+                            <Comment comment='バックアップを選択すると、そのバックアップが復元されます。' />
+                        </MenubarItem>
+                        <div className='mx-2 pb-1.5'>
+                            <BackupHistoryModal />
+                        </div>
+                    </div>
                 </MenubarContent>
             </MenubarMenu>
             <MenubarMenu>
