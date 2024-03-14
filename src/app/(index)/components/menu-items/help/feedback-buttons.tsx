957d88c72aff6b1a5bf6ef2a2d5144b667e876df
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-15 01:07:45 +0900

    github issueを利用するように

--- src/app/(index)/components/menu-items/help/feedback-buttons.tsx
+++ src/app/(index)/components/menu-items/help/feedback-buttons.tsx
@@ -3,44 +3,19 @@ import {Button} from '@/shadcn/ui/button';
 import Link from 'next/link';
 import {MenubarItem} from '@/shadcn/ui/menubar';
 
-interface FeedbackLink {
-    label: string;
-    href: string;
-}
-
-const FeedbackLinks: FeedbackLink[] = [
-    {
-        label: 'バグ報告',
-        href: '',
-    },
-    {
-        label: '意見・要望',
-        href: '',
-    },
-];
-
 export const FeedbackButtons = () => {
     return (
-        <div>
-            {FeedbackLinks.map((feedback: FeedbackLink, index: number) => {
-                return (
-                    <MenubarItem
-                        key={index}
-                        className='w-full'>
-                        <Link
-                            key={index}
-                            className='w-full'
-                            href={feedback.href}
-                            target='_blank'>
-                            <Button
-                                className='w-full'
-                                variant='hover_none'>
-                                {feedback.label}
-                            </Button>
-                        </Link>
-                    </MenubarItem>
-                );
-            })}
-        </div>
+        <MenubarItem className='w-full'>
+            <Link
+                className='w-full'
+                href='https://github.com/web-network-simulator/feedback/issues/new/choose'
+                target='_blank'>
+                <Button
+                    className='w-full'
+                    variant='hover_none'>
+                    バグ報告 / 意見・要望
+                </Button>
+            </Link>
+        </MenubarItem>
     );
 };
