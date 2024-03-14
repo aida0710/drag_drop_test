9f6a65be1d2fe09a7dab0a871df0bf7a74b52d26
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-15 00:00:20 +0900

    headerをそのまま出力するように

--- src/app/api/execute/route.ts
+++ src/app/api/execute/route.ts
@@ -13,6 +13,7 @@ export async function POST(req: Request): Promise<Response> {
         return Response.json('Postメソッド以外は許可されていません。', {status: 405});
     }
 
+    console.log(req.headers);
     console.log(req.headers.get('origin'));
     if (req.headers.get('origin') !== 'http://localhost:3000') {
         return Response.json('外部からのapiアクセスは許可されていません。', {status: 403});
