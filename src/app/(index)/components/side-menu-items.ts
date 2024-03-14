b340166a9da78e9834b7cb390eb881b5bc0431f2
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-15 00:00:40 +0900

    mapを使用するように

---
+++ src/app/(index)/components/side-menu-items.ts
@@ -0,0 +1,22 @@
+export const SideMenuItems =[
+    {
+        label: 'Gateway Node',
+        nodeType: 'Gateway'
+    },
+    {
+        label: 'L3 Switch Node',
+        nodeType: 'L3Switch'
+    },
+    {
+        label: 'L2 Switch Node',
+        nodeType: 'L2Switch'
+    },
+    {
+        label: 'Server Node',
+        nodeType: 'Server'
+    },
+    {
+        label: 'Firewall Node',
+        nodeType: 'Firewall'
+    }
+]
--- src/app/(index)/components/side-menu.tsx
+++ src/app/(index)/components/side-menu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {SideMenuItems} from '@/app/(index)/components/side-menu-items';
 
 export const SideMenu = () => {
     const onDragStart = (event: React.DragEvent<HTMLDivElement>, nodeType: string) => {
@@ -8,37 +9,16 @@ export const SideMenu = () => {
 
     return (
         <aside className='overflow-y-auto p-4'>
-            <div className='mb-4 text-sm'>アイテム一覧</div>
-            <div
-                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
-                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'Gateway')}
-                draggable>
-                Gateway Node
-            </div>
-            <div
-                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
-                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'L3Switch')}
-                draggable>
-                L3 Switch Node
-            </div>
-            <div
-                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
-                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'L2Switch')}
-                draggable>
-                L2 Switch Node
-            </div>
-            <div
-                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
-                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'Server')}
-                draggable>
-                Server Node
-            </div>
-            <div
-                className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
-                onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, 'Firewall')}
-                draggable>
-                Firewall Node
-            </div>
+            <div className='mb-4 text-sm'>ノード 一覧</div>
+            {SideMenuItems.map((item, index: number) => (
+                <div
+                    key={index}
+                    className='input-node text-md mb-4 flex h-12 cursor-grab items-center justify-center rounded border p-2'
+                    onDragStart={(event: React.DragEvent<HTMLDivElement>) => onDragStart(event, item.nodeType)}
+                    draggable>
+                    {item.label}
+                </div>
+            ))}
         </aside>
     );
 };
