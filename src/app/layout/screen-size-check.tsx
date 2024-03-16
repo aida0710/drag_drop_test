4a852c482b26fa85f87ffe62440f5aa959793061
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-16 13:16:07 +0900

    windowWidthが定義されてない場合を想定

--- src/app/layout/screen-size-check.tsx
+++ src/app/layout/screen-size-check.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import React, {useEffect, useState} from 'react';
-import {Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle} from '@/shadcn/ui/drawer';
+import {Drawer, DrawerClose, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle} from '@/shadcn/ui/drawer';
 import {Button} from '@/shadcn/ui/button';
-import {Comment} from '@/app/(index)/components/menu-items/utils/comment';
 
 interface ScreenSizeCheckProps {
     children: React.ReactNode;
@@ -12,10 +11,12 @@ interface ScreenSizeCheckProps {
 export const ScreenSizeCheck = ({children}: ScreenSizeCheckProps): React.JSX.Element => {
     const MIN_SCREEN_WIDTH: number = 1024;
 
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [windowWidth, setWindowWidth] = useState<number | null>(null);
     const [drawerOpen, setDrawerOpen] = useState(true);
 
     useEffect(() => {
+        setWindowWidth(window.innerWidth);
+
         function handleResize(): void {
             setWindowWidth(window.innerWidth);
         }
@@ -26,20 +27,23 @@ export const ScreenSizeCheck = ({children}: ScreenSizeCheckProps): React.JSX.Ele
         };
     }, []);
 
+    if (windowWidth === null) return <></>;
+
     return (
         <div>
             {windowWidth < MIN_SCREEN_WIDTH ? (
                 <Drawer open={drawerOpen}>
                     <DrawerContent>
                         <DrawerHeader>
-                                <DrawerTitle>Warning Message</DrawerTitle>
+                            <DrawerTitle>Warning Message</DrawerTitle>
                             <div className='text-lg'>
-                                <h2>お使いの端末のスクリーンサイズが {MIN_SCREEN_WIDTH}px より小さいことが検出されました。(横 {windowWidth}px)</h2>
+                                <h2>
+                                    お使いの端末のスクリーンサイズが {MIN_SCREEN_WIDTH}px より小さいことが検出されました。(横 {windowWidth}px)
+                                </h2>
                                 <p>このwebアプリケーションは画面の小さい端末に最適化が行われていません。</p>
                                 <p>一部機能が使えないなどの問題が発生する恐れがあります。</p>
                             </div>
                         </DrawerHeader>
-
                         <DrawerFooter>
                             <DrawerClose>
                                 <Button
