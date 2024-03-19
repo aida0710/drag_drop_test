33885c768d09516e3eb146f5ae8950ebd504f5fc
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-19 09:57:47 +0900

    modalによって表示できるように

--- src/app/(index)/components/menu-items/execute/execute-modal.tsx
+++ src/app/(index)/components/menu-items/execute/execute-modal.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import {DataContext} from '@/app/(index)/flow/context/data-context';
 import {Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger} from '@/shadcn/ui/dialog';
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from '@/shadcn/ui/select';
-import {ErrorModal} from '@/app/(index)/components/menu-items/execute/error-modal';
 import {BackgroundVariant, Node} from 'reactflow';
 import {EnumExecuteTypes} from '@/app/api/execute/EnumExecuteTypes';
 import {toast} from 'sonner';
@@ -27,6 +26,10 @@ export const ExecuteModal = ({
     setSelectedMethod,
 }: ExecuteModalProps) => {
     const {nodes, edges} = React.useContext(DataContext);
+    const [executeModalOpen, setExecuteModalOpen] = React.useState(false);
+
+    const [executeModalTitle, setExecuteModalTitle] = React.useState('');
+    const [executeModalMessage, setExecuteModalMessage] = React.useState('');
 
     async function execute(): Promise<void> {
         toast.promise(
@@ -41,13 +44,18 @@ export const ExecuteModal = ({
             }),
             {
                 loading: 'バックエンドサーバーに問い合わせています...',
-                success: async (res: AxiosResponse): Promise<React.JSX.Element> => {
-                    return <ErrorModal message={res.data} />;
+                success: async (res: AxiosResponse): Promise<string> => {
+                    setExecuteModalOpen(true);
+                    setExecuteModalTitle('Success');
+                    setExecuteModalMessage(res.data);
+                    return 'Success: ' + res.data;
                 },
                 error: (error: any): any | string => {
-                    console.error('Error: ', error.response.data);
                     if (error.response) {
-                        return error.response.data;
+                        setExecuteModalOpen(true);
+                        setExecuteModalTitle('Error');
+                        setExecuteModalMessage(error.response.data);
+                        return 'Error: ' + error.response.data;
                     } else {
                         return 'Unknown error occurred';
                     }
@@ -57,71 +65,94 @@ export const ExecuteModal = ({
     }
 
     return (
-        <Dialog>
-            <DialogTrigger className='p-2'>実行</DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>シミュレータ</DialogTitle>
-                    <DialogDescription>
-                        初期地点と終点を設定してください。
-                        <div className='my-3 flex gap-3'>
+        <div>
+            <Dialog>
+                <DialogTrigger className='p-2'>実行</DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>シミュレータ</DialogTitle>
+                        <DialogDescription>
+                            初期地点と終点を設定してください。
+                            <div className='my-3 flex gap-3'>
+                                <Select
+                                    defaultValue={selectedSourceNode}
+                                    onValueChange={(value: BackgroundVariant) => setSelectedSourceNode(value)}>
+                                    <SelectTrigger className='w-full'>
+                                        <SelectValue placeholder='送信元を選択してください。' />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {nodes.map((node: Node) => (
+                                            <SelectItem
+                                                key={node.id}
+                                                value={node.id}>
+                                                {node.data.ip_address}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <Select
+                                    defaultValue={selectedDestinationNode}
+                                    onValueChange={(value: BackgroundVariant) => setSelectedDestinationNode(value)}>
+                                    <SelectTrigger className='w-full'>
+                                        <SelectValue placeholder='送信先を選択してください。' />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {nodes.map((node: Node) => (
+                                            <SelectItem
+                                                key={node.id}
+                                                value={node.id}>
+                                                {node.data.ip_address}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                            </div>
                             <Select
-                                defaultValue={selectedSourceNode}
-                                onValueChange={(value: BackgroundVariant) => setSelectedSourceNode(value)}>
+                                defaultValue={selectedMethod}
+                                onValueChange={(value: string) => setSelectedMethod(value)}>
                                 <SelectTrigger className='w-full'>
-                                    <SelectValue placeholder='送信元を選択してください。' />
+                                    <SelectValue placeholder='送信方法を選択してください' />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    {nodes.map((node: Node) => (
-                                        <SelectItem
-                                            key={node.id}
-                                            value={node.id}>
-                                            {node.data.ip_address}
-                                        </SelectItem>
-                                    ))}
+                                    <SelectItem value={EnumExecuteTypes.Ping}>Ping</SelectItem>
                                 </SelectContent>
                             </Select>
-                            <Select
-                                defaultValue={selectedDestinationNode}
-                                onValueChange={(value: BackgroundVariant) => setSelectedDestinationNode(value)}>
-                                <SelectTrigger className='w-full'>
-                                    <SelectValue placeholder='送信先を選択してください。' />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {nodes.map((node: Node) => (
-                                        <SelectItem
-                                            key={node.id}
-                                            value={node.id}>
-                                            {node.data.ip_address}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                        </div>
-                        <Select
-                            defaultValue={selectedMethod}
-                            onValueChange={(value: string) => setSelectedMethod(value)}>
-                            <SelectTrigger className='w-full'>
-                                <SelectValue placeholder='送信方法を選択してください' />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value={EnumExecuteTypes.Ping}>Ping</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </DialogDescription>
-                </DialogHeader>
-                <DialogFooter className='justify-end'>
-                    <DialogClose asChild>
-                        <Button
-                            variant='ghost'
-                            onClick={(): void => {
-                                execute().then();
-                            }}>
-                            実行
-                        </Button>
-                    </DialogClose>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter className='justify-end'>
+                        <DialogClose asChild>
+                            <Button
+                                variant='ghost'
+                                onClick={(): void => {
+                                    execute().then();
+                                }}>
+                                実行
+                            </Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+            <Dialog open={executeModalOpen}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>{executeModalTitle}</DialogTitle>
+                        <DialogDescription className='text-lg'>{executeModalMessage}</DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter className='justify-end'>
+                        <DialogClose asChild>
+                            <Button
+                                onClick={(): void => {
+                                    setExecuteModalOpen(false);
+                                    setExecuteModalTitle('');
+                                    setExecuteModalMessage('');
+                                }}
+                                variant='ghost'>
+                                閉じる
+                            </Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
     );
 };
